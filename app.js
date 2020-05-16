8cf85eea127398e24b063bfc96642ffb6c16814d
Mohit Chandana <chandanamohit@gmail.com>
2020-05-17 03:39:55 +0530

    Implement heal functionality

--- app.js
+++ app.js
@@ -26,7 +26,12 @@ new Vue({
       this.monsterAttack()
     },
     heal(){
-      console.log("Healed")
+      if(this.playerHealth <= 90) {
+        this.playerHealth += 10
+      } else {
+        this.playerHealth = 100
+      }
+      this.monsterAttack()
     },
     giveUp(){
       console.log("Given up")
